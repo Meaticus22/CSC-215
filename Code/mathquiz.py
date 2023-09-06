7b11953e2219ddf9e9e1c1eb2c1a653beaabfd18
Meaticus22 <matic106@outlook.com>
2023-09-05 23:00:42 -0400

    Added new dir

---
+++ Code/mathquiz.py
@@ -0,0 +1,13 @@
+from random import randint
+num1 = randint(1, 10)
+num2 = randint(1, 10)
+question = "what is num1 * num2?"
+print(int(input("what is 4 * 3")))
+if num1 == randint:
+    print("That was not correct")
+else:
+    print("Correct!")
+if num2 == randint:
+    print("That was not correct")
+else:
+    print("Correct!")
