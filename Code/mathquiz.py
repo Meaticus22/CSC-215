e99b06eee5e7578096180a0ef35830026b5473dc
CyberMeaticus <117058929+Meaticus22@users.noreply.github.com>
2023-09-05 23:16:08 -0400

    Update mathquiz.py
    added f string and loop

--- Code/mathquiz.py
+++ Code/mathquiz.py
@@ -1,13 +1,15 @@
 from random import randint
+
 num1 = randint(1, 10)
 num2 = randint(1, 10)
-question = "what is num1 * num2?"
-print(int(input("what is 4 * 3")))
-if num1 == randint:
-    print("That was not correct")
-else:
-    print("Correct!")
-if num2 == randint:
-    print("That was not correct")
-else:
-    print("Correct!")
+question = f"What is {num1} * {num2}? "
+
+for _ in range(2):
+    answer = int(input(question))
+
+    if answer == num1 * num2:
+        print("Correct!")
+    else:
+        print("That was not correct")
+
+    print(f"The answer should have been {num1 * num2}.")
